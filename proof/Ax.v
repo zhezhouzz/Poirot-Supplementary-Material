ce3fbf96effa8886541e3d20d073a92899e3a0f1
zhouzhezz <zorichzorich@gmail.com>
2022-11-10 01:09:36 -0500

    update

--- proof/Ax.v
+++ proof/Ax.v
@@ -9,14 +9,14 @@ From Coq Require Import Lists.List.
 Import ListNotations.
 Import NormalTypeSystem.
 
-Definition state_permute {A:Type}: forall (st: string -> option A) x y (a a': A),
-    (x |-> a; y |-> a'; st) = (y |-> a'; x |-> a; st).
-Admitted.
-
 Definition exists_not_free_var_in_tm: forall e, exists x, ~ x \FVtm e.
 Admitted.
 
 Lemma closed_term_has_no_free_var: forall e x T, empty \N- e \Tin T -> ~ x \FVtm e.
+Admitted.
+
+Definition state_permute {A:Type}: forall (st: string -> option A) x y (a a': A),
+    (x |-> a; y |-> a'; st) = (y |-> a'; x |-> a; st).
 Admitted.
 
 Lemma lete_preserve_not_free: forall e x a e_a, ~ x \FVtm e_a -> ~ x \FVtm e -> ~ x \FVtm tlete a e_a e.
